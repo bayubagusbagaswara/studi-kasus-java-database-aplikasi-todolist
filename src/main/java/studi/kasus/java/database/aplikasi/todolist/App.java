e598d9aa708454dcd3ce06444c9de39adcce44a2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-10 21:59:56 +0700

    initial commit

---
+++ src/main/java/studi/kasus/java/database/aplikasi/todolist/App.java
@@ -0,0 +1,13 @@
+package studi.kasus.java.database.aplikasi.todolist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
